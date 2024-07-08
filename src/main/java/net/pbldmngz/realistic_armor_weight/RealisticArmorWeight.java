20abfa08055f7e537f77f7fd24a7aa97c8d50ee4
Pablo Domínguez <32307513+pbldmngz@users.noreply.github.com>
2024-07-08 15:06:35 +0200

    Initial setup

--- src/main/java/net/pbldmngz/realistic_armor_weight/RealisticArmorWeight.java
+++ src/main/java/net/pbldmngz/realistic_armor_weight/RealisticArmorWeight.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Very important comment
 public class RealisticArmorWeight implements ModInitializer {
 	public static final String MOD_ID = "realistic_armor_weight";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
